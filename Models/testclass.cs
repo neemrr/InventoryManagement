183030b4d207a6c0e57d43d112770f85c29128b4
naimisha rohit <naimisharohit123@gmail.com>
2021-11-23 18:03:41 +0530

    testclass.cs file add

---
+++ Models/testclass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Models
+{
+    public class testclass
+    {
+        public int testid { get; set; }
+        public string testname { get; set; }
+    }
+}
